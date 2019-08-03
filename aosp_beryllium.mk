27379704d15057cf340e9737062d80e42a8e6982
PainKiller3 <ninadpatil100@gmail.com>
2019-08-03 17:50:57 +0530

    Lets EvoX-ify
    EvoX Bringup For Poco F1
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ aosp_beryllium.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common EvoX-ify stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES_EVO := true
+TARGET_GAPPS_ARCH := arm64
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_STOCK_ARCORE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.3.5.0.PEJMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.3.5.0.PEJMIXM release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
