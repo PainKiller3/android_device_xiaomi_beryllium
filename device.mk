a64a15659effc93ca0a433b4afa8a6d3399a6f9f
PainKiller3 <ninadpatil100@gmail.com>
2019-08-23 23:24:02 +0530

    Revert "beryllium: Build PocketMode app"
    This reverts commit fe291a653688179ebc62dcdea66669ab0d0aa82b.
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- device.mk
+++ device.mk
@@ -61,10 +61,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 #Lawnchair
 PRODUCT_PACKAGES += \
     Lawnchair \
