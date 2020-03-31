b3bce014e14c2726e8cbde697dca2ea409f3dd6f
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-31 16:20:47 -0300

    beryllium: Move calib xml to device tree
    Change-Id: I0635ae2f8b741cab5514472be73f382b72f139e8
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- device.mk
+++ device.mk
@@ -54,6 +54,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_ebbg_fhd_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_ebbg_fhd_video_dsi_panel.xml \
+    $(LOCAL_PATH)/configs/qdcm_calib_data_tianma_fhd_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_tianma_fhd_video_dsi_panel.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
