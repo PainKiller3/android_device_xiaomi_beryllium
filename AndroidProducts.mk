d257c2b3ddee6756c513712e2ebda0a0f51ab5ea
1689Cedric <42259564+1689Cedric@users.noreply.github.com>
2020-05-20 11:11:10 +0800

    evo unofficial bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_beryllium.mk
+    $(LOCAL_DIR)/aosp_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_beryllium-user \
-    havoc_beryllium-userdebug \
-    havoc_beryllium-eng
+    aosp_beryllium-user \
+    aosp_beryllium-userdebug \
+    aosp_beryllium-eng
