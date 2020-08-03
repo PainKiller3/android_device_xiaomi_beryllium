5ee09bf41902e634b749365fc9171a8ce677692c
PainKiller3 <ninadpatil100@gmail.com>
2020-08-03 22:55:46 +0530

    beryllium: Update Fingerprint To Coral August Security Patch
    Change-Id: I856aa14047d4f23d7aba73795f971171e0f87f13
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- havoc_beryllium.mk
+++ havoc_beryllium.mk
@@ -19,7 +19,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=REIGNZ
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.9.0.QEJMIXM release-keys" \
