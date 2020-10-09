b4ad14ea72eb4d9e64d8a36d34bc230f3861a775
PainKiller3 <ninadpatil100@gmail.com>
2020-10-09 13:30:34 +0530

    beryllium: Update build desc to latest Miui v12.0.2.0
    * Also reflect the new source of blobs.
    
    Change-Id: Ie7880a0345145acc434e54a47228e8e99c395d22
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- havoc_beryllium.mk
+++ havoc_beryllium.mk
@@ -22,7 +22,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.1.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
