169038c6b1b004260c2ccf9dde5b16b85e5c8c6c
althafvly <althafvly@gmail.com>
2020-12-29 20:34:33 +0530

    beryllium: Build libavservices_minijail vendor variant
    Change-Id: I52c67c4abe0eaa62ce31f64e7b362182732855d9

--- device.mk
+++ device.mk
@@ -198,6 +198,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
