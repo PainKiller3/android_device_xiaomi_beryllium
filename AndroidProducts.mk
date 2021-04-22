f27887c99842da799059e256f2dc5774a3aeed5b
PainKiller3 <ninadpatil100@gmail.com>
2021-04-22 18:35:34 +0530

    beryllium: HavocOS Bringup
    Change-Id: I6c4610ea41ab5beddb49867785b963f1fc8b78cb
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/havoc_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    havoc_beryllium-user \
+    havoc_beryllium-userdebug \
+    havoc_beryllium-eng
--- havoc_beryllium.mk
+++ havoc_beryllium.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := havoc_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
