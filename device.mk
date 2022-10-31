e2547d2786aa224d5d0ec743b6ff63a0b636f7f7
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    beryllium: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    BerylliumApertureOverlay \
     BerylliumCarrierConfigOverlay \
     BerylliumFaceUnlockServiceOverlay \
     BerylliumFrameworksOverlay \
