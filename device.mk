efda10974d721896df1cc81e6ad9bcc2ca115fac
Bruno Martins <bgcngm@gmail.com>
2023-11-14 09:36:16 +0000

    beryllium: Drop MIUI poweroff charging animation
    There's no good reason to keep using this anymore.
    
    Change-Id: I5e78f2aa52a5c4b0825e01c5c479ca6bb6d5ee1f

--- device.mk
+++ device.mk
@@ -52,10 +52,6 @@ PRODUCT_PACKAGES += \
     libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-# Charger
-PRODUCT_PACKAGES += \
-    libsuspend
-
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
